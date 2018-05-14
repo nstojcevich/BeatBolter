67e32aacb5ad2e00d4f78c73a4ad185d9dac8fb2
Nikolas Stojcevich <nstojcevich@gmail.com>
2018-05-13 20:42:50 -0400

    make score write to correct file

--- src/platformer/GameManager.java
+++ src/platformer/GameManager.java
@@ -1,15 +1,13 @@
 package platformer;
 
 import java.io.*;
-import java.net.URL;
 import java.util.Scanner;
 
 class GameManager {
     private int score = 0;
     private int highScore = 0;
-    private URL url = getClass().getResource("score.txt");
     private PrintWriter writer;
-    private File scoreFile = new File(url.getPath());
+    private File scoreFile = new File("src/score.txt");
 
     GameManager() {
         readHighScoreFromFile();
@@ -52,13 +50,7 @@ class GameManager {
             Scanner sc = new Scanner(scoreFile);
             if(sc.hasNextInt())
                 newScore = sc.nextInt();
-                System.out.println("\n--BEFORE--");
-                System.out.println("newScore: " + newScore +
-                    "\nHighscore: " + highScore);
                 highScore = newScore;
-                System.out.println("--AFTER--");
-                System.out.println("newScore: " + newScore +
-                                "\nHighscore: " + highScore);
             sc.close();
         } catch(FileNotFoundException e) {
             System.err.println(e);
@@ -70,7 +62,6 @@ class GameManager {
             FileWriter write = new FileWriter(scoreFile, false);
             writer = new PrintWriter(write);
             writer.println(highScore);
-            System.out.println(scoreFile.getPath());
         } catch(IOException e) {
             System.err.println(e);
         } finally {
