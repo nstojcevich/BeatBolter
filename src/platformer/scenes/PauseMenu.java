f6d28da9b00168b13ae68e333760aa3fa36f3175
Nikolas Stojcevich <nstojcevich@gmail.com>
2018-05-18 10:14:09 -0400

    add drop shadow to pause menu buttons

--- src/platformer/scenes/PauseMenu.java
+++ src/platformer/scenes/PauseMenu.java
@@ -1,9 +1,13 @@
 package platformer.scenes;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import platformer.SceneManager;
 
 
@@ -23,14 +27,46 @@ public class PauseMenu extends VBox{
         vBox.setAlignment(Pos.CENTER);
 
         resumeGameButton = new Button();
+        DropShadow dropShadow = new DropShadow(10, Color.GRAY);
+        resumeGameButton.setEffect(dropShadow);
         resumeGameButton.setText("Resume");
         resumeGameButton.setVisible(true);
+
+        resumeGameButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                resumeGameButton.setEffect(dropShadow);
+            }
+        });
+
+        resumeGameButton.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                resumeGameButton.setEffect(null);
+            }
+        });
+
         resumeGameButton.setOnAction(actionEvent ->
                 gameScene.unpauseGame());
 
         exitToMenuButton = new Button();
         exitToMenuButton.setText("Return to Menu");
         exitToMenuButton.setVisible(true);
+
+        exitToMenuButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                exitToMenuButton.setEffect(dropShadow);
+            }
+        });
+
+        exitToMenuButton.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                exitToMenuButton.setEffect(null);
+            }
+        });
+
         exitToMenuButton.setOnAction(actionEvent -> {
             controller.switchToMenu();
             gameScene.unpauseGame();
